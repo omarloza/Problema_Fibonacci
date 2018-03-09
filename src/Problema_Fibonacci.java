56cbcd52bbcb71772cf29fa5f7c683b30273a743
OmarJuniorLozaMartinez <omar.loza.martinez@hotmail.com>
2018-03-09 16:33:35 +0100

    javadoc añadido

--- src/Problema_Fibonacci.java
+++ src/Problema_Fibonacci.java
@@ -1,10 +1,10 @@
 import java.util.Scanner;
+/**
+ * Sequencia de Fibonacci
+ * @author OMAR
+ */
 public class Problema_Fibonacci {
-		/**
-		 * Sequencia de Fibonacci
-		 * @author OMAR
-		 * @param args
-		 */
+		
 		  public static void main(String[] args) {
 			/**
 			 * Inicia l'escaner per introduir les comandes
@@ -43,18 +43,16 @@ public class Problema_Fibonacci {
 	          } 
 	     }
 	  		}
-	 
-	     public static int funcionr(int num){//sequencia de fibonacci
-	    	 /**
+		  /**
 	    	  * Aqui es crea la sequncia de fibonacci que consisteix en que apartir de 0,1 
 	    	  * es sumen els dos numero i fan el seguent com per exemple 0+1=1 llavor la sequencia sera 0,1,1 
 	    	  * seguit de 1+1=2 aixi continuament 0,1,1,2 creant la sequencia de fibonacci
-	    	  * @param if  es el caso base de salida no se produce recursividad   f(0) = 0    f(1) = 1
-	    	  * @return dona els nombres inicials 
-	    	  * @param else es el cas recursiu f(n) = f(n-1) + f(n-2)
-	    	  * return dona els nombres de la sequencia de fibonacci cridanse a si mateix continuamnet fins 
+	    	  * @param num dona els nombres inicials 
+	    	  * @return dona els nombres de la sequencia de fibonacci cridanse a si mateix continuamnet fins 
 	    	  * arribar al limit de la sequencia que se li ha donat
 	    	  */
+	     public static int funcionr(int num){//sequencia de fibonacci
+	    	
 	          if(num == 0 || num==1)
 	               return num;
 	          else
